2f34235dd91ad6cb126e8313156a1029ae8a0210
Dwayne Crooks <me@dwaynecrooks.com>
2019-06-21 06:05:35 -0400

    Prepare the code so we can use Random
    We want to be able to select a random quote when the
    "New quote" button is clicked. To do that we need to
    start using Browser.element.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,29 @@
 module Main exposing (main)
 
 
+import Browser
 import Html exposing (Html, a, blockquote, button, cite, div, footer, i, p, span, text)
 import Html.Attributes exposing (class, href, target)
 import Url.Builder exposing (crossOrigin, string)
+
+
+main : Program () Model msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+
+type alias Model =
+  { quotes : List Quote
+  , currentQuote : Quote
+  }
 
 
 type alias Quote =
@@ -12,24 +32,60 @@ type alias Quote =
   }
 
 
-main : Html msg
-main =
+init : () -> (Model, Cmd msg)
+init _ =
   let
-    quote =
+    quotes =
+      [ currentQuote
+      , { content = " Transferring your passion to your job is far easier than finding a job that happens to match your passion."
+        , author = "Seth Godin"
+        }
+      , { content = "Less mental clutter means more mental resources available for deep thinking."
+        , author = "Cal Newport"
+        }
+      , { content = "How much time he saves who does not look to see what his neighbor says or does or thinks."
+        , author = "Marcus Aurelius"
+        }
+      , { content = "You do not rise to the level of your goals. You fall to the level of your systems."
+        , author = "James Clear"
+        }
+      ]
+
+    currentQuote =
       { content = "I am not a product of my circumstances. I am a product of my decisions."
       , author = "Stephen Covey"
       }
   in
-    view quote
+    ( Model quotes currentQuote
+    , Cmd.none
+    )
+
+
+-- UPDATE
+
+
+update : msg -> Model -> (Model, Cmd msg)
+update _ model =
+  ( model
+  , Cmd.none
+  )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+  Sub.none
 
 
 -- VIEW
 
 
-view : Quote -> Html msg
-view quote =
+view : Model -> Html msg
+view { currentQuote } =
   div []
-    [ viewQuote quote
+    [ viewQuote currentQuote
     , viewAttribution
     ]
 
