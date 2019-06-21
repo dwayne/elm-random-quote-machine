5437a5396c425de31ba30133b8086d041c2a5446
Dwayne Crooks <me@dwaynecrooks.com>
2019-06-21 05:35:53 -0400

    Refactor the view

--- src/Main.elm
+++ src/Main.elm
@@ -3,40 +3,98 @@ module Main exposing (main)
 
 import Html exposing (Html, a, blockquote, button, cite, div, footer, i, p, span, text)
 import Html.Attributes exposing (class, href, target)
+import Url.Builder exposing (crossOrigin, string)
+
+
+type alias Quote =
+  { content : String
+  , author : String
+  }
 
 
 main : Html msg
 main =
+  let
+    quote =
+      { content = "I am not a product of my circumstances. I am a product of my decisions."
+      , author = "Stephen Covey"
+      }
+  in
+    view quote
+
+
+-- VIEW
+
+
+view : Quote -> Html msg
+view quote =
   div []
-    [ div [ class "mb15 quote" ]
-        [ blockquote [ class "m0 mb30" ]
-            [ p [ class "quote__content-wrapper" ]
-                [ span [ class "mr12" ]
-                    [ i [ class "fas fa-quote-left" ] [] ]
-                , text "I am not a product of my circumstances. I am a product of my decisions."
-                ]
-            , footer [ class "text-right" ]
-                [ text "— "
-                , cite [ class "quote__author" ]
-                    [ text "Stephen Covey" ]
-                ]
-            ]
-        , div [ class "flex" ]
-            [ div [ class "mr10" ]
-                [ a [ class "icon-button", href "https://twitter.com/intent/tweet?hashtags=quotes&text=%22I%20am%20not%20a%20product%20of%20my%20circumstances.%20I%20am%20a%20product%20of%20my%20decisions.%22%20~%20Stephen%20Covey", target "_blank" ]
-                    [ i [ class "fab fa-twitter" ] [] ]
-                ]
-            , div []
-                [ a [ class "icon-button", href "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=Stephen%20Covey&content=I%20am%20not%20a%20product%20of%20my%20circumstances.%20I%20am%20a%20product%20of%20my%20decisions.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fdocs%2Fen%2Fshare_button", target "_blank" ]
-                    [ i [ class "fab fa-tumblr" ] [] ]
-                ]
-            , div [ class "push-right" ]
-                [ button [ class "button" ] [ text "New quote" ] ]
-            ]
-        ]
-    , footer [ class "attribution" ]
-        [ text "by "
-        , a [ class "attribution__link", href "https://github.com/dwayne/" ]
-            [ text "dwayne" ]
-        ]
+    [ viewQuote quote
+    , viewAttribution
+    ]
+
+
+viewQuote : Quote -> Html msg
+viewQuote quote =
+  div [ class "mb15 quote" ]
+      [ blockquote [ class "m0 mb30" ]
+          [ p [ class "quote__content-wrapper" ]
+              [ span [ class "mr12" ] [ i [ class "fas fa-quote-left" ] [] ]
+              , text quote.content
+              ]
+          , footer [ class "text-right" ]
+              [ text "— "
+              , cite [ class "quote__author" ] [ text quote.author ]
+              ]
+          ]
+      , div [ class "flex" ]
+          [ div [ class "mr10" ]
+              [ viewIconButton "twitter" (twitterUrl quote) ]
+          , div []
+              [ viewIconButton "tumblr" (tumblrUrl quote) ]
+          , div [ class "push-right" ]
+              [ button [ class "button" ] [ text "New quote" ] ]
+          ]
+      ]
+
+
+viewIconButton : String -> String -> Html msg
+viewIconButton name url =
+  a [ class "icon-button", href url, target "_blank" ]
+      [ i [ class ("fab fa-" ++ name) ] [] ]
+
+
+viewAttribution : Html msg
+viewAttribution =
+  footer [ class "attribution" ]
+      [ text "by "
+      , a [ class "attribution__link", href "https://github.com/dwayne/" ]
+          [ text "dwayne" ]
+      ]
+
+
+-- HELPERS
+
+
+twitterUrl : Quote -> String
+twitterUrl { content, author } =
+  let
+    tweet = "\"" ++ content ++ "\" ~ " ++ author
+  in
+    crossOrigin "https://twitter.com"
+      [ "intent", "tweet" ]
+      [ string "hashtags" "quotes"
+      , string "text" tweet
+      ]
+
+
+tumblrUrl : Quote -> String
+tumblrUrl { content, author } =
+  crossOrigin "https://www.tumblr.com"
+    [ "widgets", "share", "tool" ]
+    [ string "posttype" "quote"
+    , string "tags" "quotes"
+    , string "content" content
+    , string "caption" author
+    , string "canonicalUrl" "https://www.tumblr.com/docs/en/share_button"
     ]
