cee9a531432dd119d6679376ee65376c5a68216e
Dwayne Crooks <me@dwaynecrooks.com>
2019-06-21 04:32:07 -0400

    Convert the static version to Elm
    I used http://mbylstra.github.io/html-to-elm/ to generate the Elm
    code which I then edited to my liking.
    
    There was one hitch. The quotation in the static version had extra
    whitespace to its right causing me to use an mr5 class. However,
    in the Elm version there was no extra whitespace so I had to change
    it to use an mr12 class. I didn't notice the extra whitespace before
    which I didn't want anyway.
    
    In short, Elm helped me to catch a mistake in my HTML.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,42 @@
 module Main exposing (main)
 
 
-import Html exposing (Html, text)
+import Html exposing (Html, a, blockquote, button, cite, div, footer, i, p, span, text)
+import Html.Attributes exposing (class, href, target)
 
 
 main : Html msg
 main =
-  text "Hello, world!"
+  div []
+    [ div [ class "mb15 quote" ]
+        [ blockquote [ class "m0 mb30" ]
+            [ p [ class "quote__content-wrapper" ]
+                [ span [ class "mr12" ]
+                    [ i [ class "fas fa-quote-left" ] [] ]
+                , text "I am not a product of my circumstances. I am a product of my decisions."
+                ]
+            , footer [ class "text-right" ]
+                [ text "— "
+                , cite [ class "quote__author" ]
+                    [ text "Stephen Covey" ]
+                ]
+            ]
+        , div [ class "flex" ]
+            [ div [ class "mr10" ]
+                [ a [ class "icon-button", href "https://twitter.com/intent/tweet?hashtags=quotes&text=%22I%20am%20not%20a%20product%20of%20my%20circumstances.%20I%20am%20a%20product%20of%20my%20decisions.%22%20~%20Stephen%20Covey", target "_blank" ]
+                    [ i [ class "fab fa-twitter" ] [] ]
+                ]
+            , div []
+                [ a [ class "icon-button", href "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=Stephen%20Covey&content=I%20am%20not%20a%20product%20of%20my%20circumstances.%20I%20am%20a%20product%20of%20my%20decisions.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fdocs%2Fen%2Fshare_button", target "_blank" ]
+                    [ i [ class "fab fa-tumblr" ] [] ]
+                ]
+            , div [ class "push-right" ]
+                [ button [ class "button" ] [ text "New quote" ] ]
+            ]
+        ]
+    , footer [ class "attribution" ]
+        [ text "by "
+        , a [ class "attribution__link", href "https://github.com/dwayne/" ]
+            [ text "dwayne" ]
+        ]
+    ]
