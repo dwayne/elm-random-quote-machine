a6a905e694871133fb1c252d4d47406170f32ab1
Dwayne Crooks <me@dwaynecrooks.com>
2022-09-15 22:48:29 -0400

    Refactor the build and clean scripts

--- prototype/bin/clean.sh
+++ prototype/bin/clean.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf \
-  "$PROTOTYPE/node_modules" \
-  "$PROTOTYPE/index.css" \
-  "$PROTOTYPE/index.css.map"
+cd "$PROTOTYPE" && rm -rf node_modules index.css index.css.map
