b38f851804b6b43c75b7c477415e5cef4717a035
Dwayne Crooks <me@dwaynecrooks.com>
2024-01-03 06:48:43 -0400

    Add flake.nix and remove shell.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A developer shell for working on elm-random-quote-machine.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "elm-random-quote-machine";
+
+          packages = with pkgs.elmPackages; [
+            elm
+            elm-format
+            elm-optimize-level-2
+            elm-review
+            elm-test
+            pkgs.caddy
+            pkgs.sass
+            pkgs.shellcheck
+            pkgs.terser
+          ];
+
+          shellHook =
+            ''
+            export project="$PWD"
+            export build="$project/.build"
+            export PATH="$project/bin:$PATH"
+            '';
+        };
+      }
+    );
+}
