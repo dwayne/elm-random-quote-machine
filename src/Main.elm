864550c47f6fb757a00421b35bfd691739d3ce55
Dwayne Crooks <me@dwaynecrooks.com>
2024-01-06 12:51:31 -0400

    Refactor Elm: quote

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,37 @@ module Main exposing (main)
 
 
 import Html as H
+import Html.Attributes as HA
 
 
 main : H.Html msg
 main =
-    H.text "Hello, world!"
+    viewQuote
+        { text = "You become what you believe."
+        , author = "Oprah Winfrey"
+        }
+
+
+-- QUOTE
+
+
+type alias Quote =
+    { text : String
+    , author : String
+    }
+
+
+viewQuote : Quote -> H.Html msg
+viewQuote { text, author } =
+    H.blockquote [ HA.class "quote" ]
+        [ H.p [ HA.class "quote__text" ] [ H.text text ]
+        , H.footer [ HA.class "quote__footer" ]
+            [ H.text <| mdash ++ " "
+            , H.cite [ HA.class "quote__author" ] [ H.text author ]
+            ]
+        ]
+
+
+mdash : String
+mdash =
+    "\u{2014}"
